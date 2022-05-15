b8c05cde3f971dafa1a7642204e89636b6f8b12c
yobi213 <yobi@yobis-MacBook-Pro.local>
2022-05-15 22:07:29 +0900

    15 데이터 수정하기

--- src/main/java/com/example/springpractice/controller/ArticleController.java
+++ src/main/java/com/example/springpractice/controller/ArticleController.java
@@ -76,4 +76,24 @@ public class ArticleController {
         return "articles/edit";
     }
 
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+
+        // 1. DTO를 entity로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. entity를 DB로 저장
+        // 2-1. DB에서 기존 데이터를 가져옴
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터가 있으면 값을 갱신
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
 }
