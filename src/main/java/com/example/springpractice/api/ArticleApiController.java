873da10c0d87c872168ebdbf9f237bb07ce92031
yobi213 <yobi@yobis-MacBook-Pro.local>
2022-05-16 23:20:44 +0900

    19 HTTP와 RestController

---
+++ src/main/java/com/example/springpractice/api/ArticleApiController.java
@@ -0,0 +1,76 @@
+package com.example.springpractice.api;
+
+import com.example.springpractice.dto.ArticleForm;
+import com.example.springpractice.entity.Article;
+import com.example.springpractice.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    // GET
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    // POST
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
+    }
+    // PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id,
+                                          @RequestBody ArticleForm dto) {
+        // 1: DTO -> 엔티티
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        // 2: 타겟 조회
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 3: 잘못된 요청 처리
+        if (target == null || id != article.getId()) {
+            // 400, 잘못된 요청 응답
+            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 4: 업데이트 및 정상 응답(200)
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    // DELETE
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        // 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 잘못된 요청 처리
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 대상 삭제
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
--- src/main/java/com/example/springpractice/entity/Article.java
+++ src/main/java/com/example/springpractice/entity/Article.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // DB가 해당 객체를 인식
 @AllArgsConstructor
@@ -18,7 +15,7 @@ import javax.persistence.Id;
 public class Article {
 
     @Id // 대표값을 지정
-    @GeneratedValue // 1,2,3, 자동 생성 어노테이션
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 1,2,3, 자동 생성 어노테이션
     private Long id;
 
     @Column
@@ -27,4 +24,10 @@ public class Article {
     @Column
     private String content;
 
+    public void patch(Article article) {
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
+    }
 }
